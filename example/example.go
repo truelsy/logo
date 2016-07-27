84a1e51d0229a4b98f35dfc3322c566436d0387e
truelsy <truelsy@gmail.com>
2016-07-27 09:24:15 +0900

    예제 소스 추가

---
+++ example/example.go
@@ -0,0 +1,35 @@
+// example.go
+package main
+
+import (
+	"time"
+
+	"github.com/truelsy/logo"
+)
+
+func main() {
+	logo.Init(&logo.Environment{
+		LogLevel:       logo.LEVEL_DEBUG,
+		LogPath:        "/tmp/logo/",
+		RotateFileSize: 1024 * 10, // 10K
+		LogKeepTime:    time.Hour * 6,
+		WriteConsole:   true,
+	})
+	defer logo.Close()
+
+	logo.Debug("Debug")
+	logo.Debugf("Debugf(%v)", 123)
+	logo.CDebug(logo.FG_GREEN, "CDebug")
+	logo.CDebugf(logo.FG_LIGHT_GREEN, "CDebugf(%v)", "hello")
+
+	logo.Info("Info")
+	logo.Infof("Infof(%v)", 456)
+	logo.CInfo(logo.FG_YELLOW, "CInfo")
+	logo.CInfof(logo.FG_LIGHT_GREEN, "CInfof(%v)", "logo")
+
+	logo.Warn("Warn")
+	logo.Warnf("Warnf(%v)", 789)
+
+	logo.Error("Error")
+	logo.Errorf("Error(%v)", 890)
+}
